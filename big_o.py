db6c578a996aef84154306cda8bcdca69f65df79
wh <sunfortea123@gmail.com>
2025-04-14 05:06:01 +0200

    new chagne

--- big_o.py
+++ big_o.py
@@ -1,3 +1,7 @@
+# o(1) constant time
+def add_me(n):
+    return n+n
+
 # o(n)
 def print_n(n):
     for i in range(n):
@@ -9,3 +13,17 @@ def print_2n(n):
     for j in range(n):
         print(j)
 
+# o(n^2)
+def print_nn(n):
+    for i in range(n):
+        for j in range(n):
+            print(i,j)
+
+def print_nn(n):
+    for i in range(n):
+        for j in range(n):
+            print(i,j)
+    for k in range(n): # n^2 + n = n^2
+        print(k)
+
+# o(log n)
